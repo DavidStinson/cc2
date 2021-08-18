e0683df565a55cb762ffb92c431e97278f3c2889
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-08-17 23:25:17 -0500

    add dates to feedings

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -19,6 +20,9 @@ class Cat(models.Model):
 
   def get_absolute_url(self):
       return reverse("cats_detail", kwargs={'cat_id': self.id})
+  
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 class Feeding(models.Model):
   date = models.DateField('Feeding Date')
@@ -34,3 +38,6 @@ class Feeding(models.Model):
   def __str__(self):
     # Nice method for obtaining the friendly value of a Field.choice
     return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+    ordering = ['-date']
